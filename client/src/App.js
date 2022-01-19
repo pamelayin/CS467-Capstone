9b64188b4f9f1759518959be1c21eeb93825723c
Paul Leone <35977957+pleone55@users.noreply.github.com>
2022-01-19 09:29:40 -0800

    manage account, edit profile, quizlist framework commit, fix gitignore and readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/pages/Home';
-
+import ManageAccount from './components/pages/ManageAccount';
+import EditProfile from './components/pages/EditProfile';
+import QuizList from "./components/pages/QuizList";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
