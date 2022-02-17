42525316fb339c9d3341315c9d180b30e9964273
Paul Leone <35977957+pleone55@users.noreply.github.com>
2022-02-17 14:03:22 -0800

    Implemented solution for checkbox to determine checked and unchecked answers and to combine answers into one object. Countdown time interval works

--- client/src/components/pages/RespondentQuiz.js
+++ client/src/components/pages/RespondentQuiz.js
@@ -9,55 +9,68 @@ const RespondentQuiz = () => {
     const { error, respondent, quiz } = respondentState;
     const { userId, quizId } = useParams();
 
-    const [questionsAnswered, setQuestionsAnswered] = useState([{
-        question_id: '',
-        answerGiven: ''
-    }]);
+    const [questionsAnswered, setQuestionsAnswered] = useState([]);
 
-    // let time = quiz && quiz.timeLimit;
+    let time = quiz && quiz.timeLimit;
+    const [stateTimeLimit, setStateTimeLimit] = useState(0);
 
-    // const [stateTimeLimit, setStateTimeLimit] = useState(time);
-
-    // const onTimeLimitChange = useCallback(() => {
-    //     setInterval(() => setStateTimeLimit(stateTimeLimit - 1), 10000);
-    // },[stateTimeLimit],
-    // );
+    const onTimeLimitChange = useCallback(() => {
+        setInterval(() => setStateTimeLimit(stateTimeLimit - 1), 60000);
+    },[stateTimeLimit]);
 
     // const clearTimer = () => {
     //     clearInterval(stateTimeLimit);
     // }
 
     useEffect(() => {
+        setStateTimeLimit(time);
+    }, [time])
+
+    useEffect(() => {
         if(error) {
             console.log(error);
         }
-        // onTimeLimitChange();
+        onTimeLimitChange();
         getRespondentQuiz(respondentDispatch, userId, quizId);
-    }, [respondentDispatch, error, userId, quizId]);
+    }, [respondentDispatch, error, userId, quizId, onTimeLimitChange]);
 
-    const onChange = (e, q, i) => {
+    const onChange = e => {
+        let answerGiven = questionsAnswered;
+
         if(e.target.checked) {
-            if(!questionsAnswered[i]['answerGiven'].includes(e.target.value)) {
-                setQuestionsAnswered([...questionsAnswered, 
-                    { ...questionsAnswered[questionsAnswered.length], 
-                        question_id: q._id, 
-                        answerGiven: e.target.value
-                    }
-                ]);
-            } else {
-                setQuestionsAnswered(questionsAnswered.filter(answerGiven => 
-                    answerGiven.answerGiven !== e.target.value));
+            if(!questionsAnswered.includes(e.target.value)) {
+                setQuestionsAnswered([...questionsAnswered, ({
+                    question_id: e.target.id,
+                    answerGiven: e.target.value
+                })]);
             }
+        } else {
+            answerGiven = answerGiven.filter(el => el.answerGiven !== e.target.value)
+            setQuestionsAnswered(answerGiven);
         }
+    }
+
+    //https://stackoverflow.com/questions/57703415/how-to-merge-array-of-objects-if-duplicate-values-are-there-if-key-is-common-th
+    const mergeQuestionIds = e => {
+        e.preventDefault();
+
+        const data = Object.values(questionsAnswered.reduce((a, {question_id, answerGiven}) => {
+            a[question_id] = a[question_id] || { question_id, answerGiven: new Set() };
+            a[question_id].answerGiven.add(answerGiven);
+            return a;
+        }, {})).map(({question_id, answerGiven}) => ({ question_id, answerGiven: [...answerGiven].join(",")}));
+
+        return data;
     }
 
     const onSubmit = e => {
         e.preventDefault();
-
+        const data = mergeQuestionIds(e);
         console.log(questionsAnswered);
+        console.log(data);
 
-        takeQuiz(respondentDispatch, {
-            questionsAnswered},
+        takeQuiz(respondentDispatch, 
+            { quizzes: data },
             userId,
             quizId
         );
@@ -67,7 +80,7 @@ const RespondentQuiz = () => {
         <Container>
             <Container>
                 <h1 className='shadow-sm p-3 text-center rounded' style={{ color: 'black' }}>{quiz && quiz.title}</h1><span style={{ display: 'inline'}}>
-                    Time Limit: {quiz && quiz.timeLimit}
+                    Time Limit: {stateTimeLimit}
                 </span>
             </Container>
             {quiz && quiz.questions.map((q, i) => (
@@ -79,22 +92,23 @@ const RespondentQuiz = () => {
                         </Form.Group>
                         <Form.Group>
                             {q.answerOptions.map((a, index) => (
-                                q.questionType === 'FR' ? (
+                                q.questionType === 'FA' ? (
                                     <Form.Control
                                         key={index}
                                         type='text'
-                                        name='answerGiven'
+                                        name='questionsAnswered'
                                         value={a}
-                                        onChange={e => onChange(e, q, i)}
+                                        onChange={e => onChange(e)}
                                     />
                                 ) : (
                                 <Form.Check
                                     type={q.questionType === 'TF' || q.questionType === 'SC' ? 'radio' : 'checkbox'}
                                     key={index}
                                     label={a}
+                                    id={q._id}
                                     value={a}
-                                    name='answerGiven'
-                                    onChange={e => onChange(e, q, i)}
+                                    name='questionsAnswered'
+                                    onChange={e => onChange(e)}
                                 />
                                 )
                             ))}
