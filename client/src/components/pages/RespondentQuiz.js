adbb3b2037c15953abe21769b9607dce20be468b
Paul Leone <35977957+pleone55@users.noreply.github.com>
2022-03-07 20:07:59 -0800

    removing commented out code. not needed.

--- client/src/components/pages/RespondentQuiz.js
+++ client/src/components/pages/RespondentQuiz.js
@@ -167,14 +167,6 @@ const RespondentQuiz = () => {
         }
     }, []);
 
-    // useEffect(() => {
-    //     window.addEventListener('unload', alertUser);
-    //     window.onunload = submitTest;
-    //     return () => {
-    //         window.removeEventListener('unload', alertUser)
-    //     }
-    // }, []);
-
     useEffect(() => {
         if(respondent && respondent.quizzes) {
             const quiz = respondent.quizzes.find(quiz => quiz.quiz_id === quizId);
